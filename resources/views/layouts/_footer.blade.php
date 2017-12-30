ca1a6481b045b966f007d0dfcfd49bcbe590a3d7
Roya <416706545@qq.com>
2017-12-30 15:29:21 +0000

    Finish layout and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+  <footer class="footer">
+    <small class="slogon">
+      <img class="brand-icon" src="https://wechat2.hiwehiwe.com/image/test.jpg">
+      <a href="https://yousails.com/">
+        Roya | 80后码农
+      </a>
+    </small>
+    <nav>
+      <ul>
+        <li><a href="{{ route('about') }}">关于</a></li>
+      </ul>
+    </nav>
+  </footer>
+</div>
