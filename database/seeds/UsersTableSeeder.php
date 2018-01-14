09850a891c97ff34668b255d3e1fbd66f92ae1dd
Roya <416706545@qq.com>
2018-01-14 14:40:50 +0000

    Add account activations

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -21,6 +21,7 @@ class UsersTableSeeder extends Seeder
         $user->email = '17364515813@163.com';
         $user->password = bcrypt('123456');
         $user->is_admin = true;
+        $user->activated = true;
         $user->save();
     }
 }
